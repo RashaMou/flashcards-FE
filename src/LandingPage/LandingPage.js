b22b69aa1084452c791601892c2d9bb299767369
Rasha Moumneh <rasha@fastmail.com>
2020-06-04 17:31:47 -0400

    App skeleton

---
+++ src/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h2>Landing Page</h2>
+      <h4>Marketing page - also contains links to sign up and login</h4>
+    </div>
+  );
+};
+
+export default LandingPage;
